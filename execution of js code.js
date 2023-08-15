ce35fb7d54b39b5f293d7dc4e516815c67eb32b2
Shiv Kant Kumar <95178381+golu098@users.noreply.github.com>
2023-08-15 18:49:21 +0530

    Create execution of js code.js

---
+++ execution of js code.js
@@ -0,0 +1,29 @@
+
+console.log('Welcome to my repo');
+
+// how js is executed in compiler
+
+
+// 1) Memory creation
+// we are allocating memory to all the variables and functions inside the global space
+// we are allocating undefined to these variables
+// for function we are just storing whole blocks of function
+
+
+//  2) code execution phase
+// program is execution goes line by line
+// replace undefined with value
+
+//   it becomes too complex 
+// so stack came i.e.call stack
+// --> call stack maintains the order of execution of execution contexts
+
+
+// call stack is known by 
+// 1) execution contexts stack
+// 2) program stack
+// 3) control stack
+// 4) runtime stack
+// 5) machine stack
+
+ 
