0097831f78da427e376ef4fc77e7bcb0222e2a88
Shiv Kant Kumar <95178381+golu098@users.noreply.github.com>
2023-08-19 15:26:56 +0530

    Create closure.js

---
+++ closure.js
@@ -0,0 +1,10 @@
+// function along with lexical environment is known as clousre
+function init() {
+  var name = "closure"; // name is a local variable created by init
+  function displayName() {
+    // displayName() is the inner function, that forms the closure
+    console.log(name); // use variable declared in the parent function
+  }
+  displayName();
+}
+init();
